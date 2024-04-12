e774284f4a3367fdfa4020ef870d4808cda30ef1
Josuesp1620 <josuesp1620@gmail.com>
2024-04-12 21:46:00 +0200

    Add User Auth

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ os.environ["ENABLE_SQL_ENDPOINT"] = "0"
 def_cfg = file_query.api.get_default_config()
 
 cfg = dataclasses.replace(def_cfg, enable_sql_endpoint=True, data_path="data/", default_engine="duckdb")
-sti = file_query.api.init_lakeapi(app, False, cfg, "config.yml")
+sti = file_query.api.init_lakeapi(app, True, cfg, "config.yml")
 
 # uvicorn app:app --host 0.0.0.0 --port 3072
